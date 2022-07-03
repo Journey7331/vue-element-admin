36660010a36902bf85739ba00d3d8f98e073087f
Journey7331 <66991472+Journey7331@users.noreply.github.com>
2022-07-03 22:34:28 +0800

    commit 220703

---
+++ src/views/data-analyze/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "DataAnalyze"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/follow-up/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "FollowUp"
+}
+</script>
+
+<style scoped>
+
+</style>
